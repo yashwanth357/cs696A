1ec49e3a60885c77872f0fa1ca21742cfcc2d6d7
yashwanth357 <yashuyashwanth692@gmail.com>
2024-10-15 21:11:36 -0400

    all squares

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -2,20 +2,20 @@ export default function Square() {
   return (
   <>
   <div className="row">
-   <button className="square">X</button> 
-   <button className="square">X</button>
-   <button className="square">X</button> 
+   <button className="square">1</button> 
+   <button className="square">1</button>
+   <button className="square">1</button> 
    </div>
    <div className="row">
-   <button className="square">X</button> 
-   <button className="square">X</button>
-   <button className="square">X</button> 
+   <button className="square">1</button> 
+   <button className="square">1</button>
+   <button className="square">1</button> 
    </div>
 
    <div className="row">
-   <button className="square">X</button> 
-   <button className="square">X</button>
-   <button className="square">X</button> 
+   <button className="square">1</button> 
+   <button className="square">1</button>
+   <button className="square">1</button> 
    </div>
   </>
   )
