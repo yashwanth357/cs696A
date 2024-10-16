2356fc31fbcdfeff7f0a728222640882e1c5a026
yashwanth357 <yashuyashwanth692@gmail.com>
2024-10-15 23:29:51 -0400

    added moves

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -77,6 +77,24 @@ export default function Game() {
     setXIsNext(!xIsNext);
   }
 
+  function jumpTo(nextMove) {
+    // TODO
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
 
   return (
     <div className="game">
@@ -86,7 +104,7 @@ export default function Game() {
         <Board />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+      <ol>{moves}</ol>
       </div>
     </div>
   );
