d24fb8f434f2f5ccd153d2325d66889dbd361141
yashwanth357 <yashuyashwanth692@gmail.com>
2024-10-15 23:23:09 -0400

    added usestate in game

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -66,6 +66,8 @@ function Board() {
   )
 }
 export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
   return (
     <div className="game">
       <div className="game-board">
