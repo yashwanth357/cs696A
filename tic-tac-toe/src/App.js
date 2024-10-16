2ccd2539a899ca0eff1152a2aa78bf2aa7b78687
yashwanth357 <yashuyashwanth692@gmail.com>
2024-10-15 23:38:24 -0400

    added currentsq

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -56,11 +56,14 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
 
-  const currentSquares = history[history.length - 1];
+
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
   }
 
