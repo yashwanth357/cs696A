612c48b66de2fe51beae0e8821d4823456e9e9b8
yashwanth357 <yashuyashwanth692@gmail.com>
2024-10-15 23:24:29 -0400

    added currentsquares

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -68,6 +68,8 @@ function Board() {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
   return (
     <div className="game">
       <div className="game-board">
