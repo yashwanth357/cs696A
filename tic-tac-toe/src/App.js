2fc9381e7d2cdbe624143460fd3c400d7d75d094
yashwanth357 <yashuyashwanth692@gmail.com>
2024-10-15 23:25:39 -0400

    added handleplay

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -69,10 +69,16 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
+  function handlePlay(nextSquares) {
+    // TODO
+  }
+
 
   return (
     <div className="game">
       <div className="game-board">
+      <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+
         <Board />
       </div>
       <div className="game-info">
